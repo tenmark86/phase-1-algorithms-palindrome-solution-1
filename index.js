3a934ea640e28349dc888329d34323ae3b6f7a7c
tenmark86e <tenmark86@gmail.com>
2022-08-24 13:53:28 +0300

    Done Palindrome Lab Solution 1 assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  
+    if(word.length < 2) return true
+  
+    let start = 0
+    let end = word.length - 1
+  
+    while(start <= end){
+      if(word[start] != word[end]){
+        return false
+      }
+      start++
+      end--
+    }
+  
+      return true
+    
+  
+  }
+
 
 /* 
-  Add your pseudocode here
+  loop through half of the string
+  check if first and last string are same
+  returns true or false
 */
 
 /*
-  Add written explanation of your solution here
+The length of the string is calculated using the length property.
+The for loop is used to iterate up to the half of the string. The if condition is used to check if the first and the corresponding last characters are the same. This loop continues till the half of the string.
+During the iteration, if any character of the string, when compared with its corresponding last string is not equal, the string is not considered a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
